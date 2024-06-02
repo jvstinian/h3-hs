aa35f5457cc58d1b91f7834d5e6ef26886f274e5
Justin Smith <jvstinian@gmail.com>
2024-06-02 13:02:38 -0400

    Adding comments.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # h3-hs
 
+This library provides Haskell bindings to the [H3 API](https://h3geo.org/docs/api/indexing). 
+
--- src/H3/Internal/FFI.hs
+++ src/H3/Internal/FFI.hs
@@ -5,7 +5,9 @@ module H3.Internal.FFI
   , radsToDegs
   ) where
 
+-- |degsToRads converts from degrees to radians.
 foreign import capi "h3/h3api.h degsToRads" degsToRads :: Double -> Double
 
+-- |radsToDegs converts from radians to degrees
 foreign import capi "h3/h3api.h radsToDegs" radsToDegs :: Double -> Double
 
