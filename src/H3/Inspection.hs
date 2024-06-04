a3e337659fcf4ea8acc59306cb888d602aa4d6bd
Justin Smith <jvstinian@gmail.com>
2024-06-03 21:54:57 -0400

    Adding Inspection API methods.

--- src/H3/Inspection.hs
+++ src/H3/Inspection.hs
@@ -1,7 +1,12 @@
 module H3.Inspection
-  ( h3ToString
+  ( getResolution
+  , getBaseCellNumber
   , stringToH3
+  , h3ToString
   , isValidCell
+  , isResClassIII
+  , isPentagon
+  , getIcosahedronFaces
   ) where
 
 import H3.Internal.H3Api 
@@ -9,7 +14,13 @@ import H3.Internal.H3Api
   , H3Index
   , c2hs_h3ToString
   , c2hs_stringToH3 )
-import H3.Internal.FFI (isValidCell)
+import H3.Internal.FFI 
+  ( getResolution
+  , getBaseCellNumber
+  , isValidCell
+  , isResClassIII
+  , isPentagon
+  , hsGetIcosahedronFaces )
 import H3.Internal.Utils (toEither)
 
 stringToH3 :: String -> Either H3ErrorCodes H3Index
@@ -17,4 +28,8 @@ stringToH3 = toEither . c2hs_stringToH3
 
 h3ToString :: H3Index -> Either H3ErrorCodes String
 h3ToString = toEither . c2hs_h3ToString
+
+-- TODO: Do we remove the -1s?
+getIcosahedronFaces :: H3Index -> Either H3ErrorCodes [Int]
+getIcosahedronFaces = toEither . hsGetIcosahedronFaces
 
--- src/H3/Internal/FFI.hs
+++ src/H3/Internal/FFI.hs
@@ -3,10 +3,22 @@
 module H3.Internal.FFI
   ( degsToRads
   , radsToDegs
+  , getResolution
+  , getBaseCellNumber
   , isValidCell
+  , isResClassIII
+  , isPentagon
+  , hsGetIcosahedronFaces 
   ) where
 
-import H3.Internal.H3Api (H3Index)
+-- import System.IO.Unsafe (unsafePerformIO)
+import H3.Internal.H3Api (H3Index, H3Error)
+import Foreign.C.Types (CInt)
+import System.IO.Unsafe (unsafePerformIO)
+import Foreign.Ptr (Ptr)
+import Foreign.Storable (Storable(peek))
+import Foreign.Marshal.Alloc (alloca)
+import Foreign.Marshal.Array (allocaArray, peekArray)
 
 
 -- |degsToRads converts from degrees to radians.
@@ -15,5 +27,35 @@ foreign import capi "h3/h3api.h degsToRads" degsToRads :: Double -> Double
 -- |radsToDegs converts from radians to degrees
 foreign import capi "h3/h3api.h radsToDegs" radsToDegs :: Double -> Double
 
+-- Inspection
+
+foreign import capi "h3/h3api.h getResolution" getResolution :: H3Index -> Int
+
+foreign import capi "h3/h3api.h getBaseCellNumber" getBaseCellNumber :: H3Index -> Int
+
 foreign import capi "h3/h3api.h isValidCell" isValidCell :: H3Index -> Int
+
+foreign import capi "h3/h3api.h isResClassIII" isResClassIII :: H3Index -> Int
+
+foreign import capi "h3/h3api.h isPentagon" isPentagon :: H3Index -> Int
+
+foreign import capi "h3/h3api.h maxFaceCount" c_maxFaceCount :: H3Index -> Ptr CInt -> IO H3Error
+
+foreign import capi "h3/h3api.h getIcosahedronFaces" c_getIcosahedronFaces :: H3Index -> Ptr CInt -> IO H3Error
+
+hsGetIcosahedronFaces :: H3Index -> (H3Error, [Int])
+hsGetIcosahedronFaces h3index = 
+  unsafePerformIO $ alloca $ \countptr -> do
+    h3error <- c_maxFaceCount h3index countptr
+    if h3error == 0
+    then do
+      count <- fromIntegral <$> peek countptr
+      allocaArray count $ \facesptr -> do
+        out3error <- c_getIcosahedronFaces h3index facesptr
+        if out3error == 0
+        then do
+          faces <- map fromIntegral <$> peekArray count facesptr
+          return (out3error, faces)
+        else return (out3error, [])
+    else return (h3error, [])
 
