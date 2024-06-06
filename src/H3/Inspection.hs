f4c71f0d49f41b1ddca8eac2844f98c7203e5291
jvstinian <jvstinian@gmail.com>
2024-06-06 23:54:45 +0000

    Inspection API (#3)
    * Adding Indexing API
    * Adding test cases

--- src/H3/Inspection.hs
+++ src/H3/Inspection.hs
@@ -1,7 +1,12 @@
 module H3.Inspection
-  ( h3ToString
+  ( getResolution
+  , getBaseCellNumber
   , stringToH3
+  , h3ToString
   , isValidCell
+  , isResClassIII
+  , isPentagon
+  , getIcosahedronFaces
   ) where
 
 import H3.Internal.H3Api 
@@ -9,14 +14,25 @@ import H3.Internal.H3Api
   , H3Index
   , c2hs_h3ToString
   , c2hs_stringToH3 )
-import H3.Internal.FFI (isValidCell)
+import H3.Internal.FFI 
+  ( getResolution
+  , getBaseCellNumber
+  , isValidCell
+  , isResClassIII
+  , isPentagon
+  , hsGetIcosahedronFaces )
 import H3.Internal.Utils (toEither)
 
 -- |Converts the string representation to the H3Index (Word64) representation.
 stringToH3 :: String -> Either H3ErrorCodes H3Index
 stringToH3 = toEither . c2hs_stringToH3
 
--- |Converts the H3Index representation of the index to the string representation.
+-- |Converts the H3Index representation to the string representation.
 h3ToString :: H3Index -> Either H3ErrorCodes String
 h3ToString = toEither . c2hs_h3ToString
+
+-- | Return all icosahedron faces intersected by a given H3 index.  Faces are represented as integers from 0-19, inclusive. 
+--   The array is sparse, and empty (no intersection) array values are represented by -1.
+getIcosahedronFaces :: H3Index -> Either H3ErrorCodes [Int]
+getIcosahedronFaces = toEither . hsGetIcosahedronFaces
 
--- src/H3/Internal/FFI.hs
+++ src/H3/Internal/FFI.hs
@@ -3,10 +3,21 @@
 module H3.Internal.FFI
   ( degsToRads
   , radsToDegs
+  , getResolution
+  , getBaseCellNumber
   , isValidCell
+  , isResClassIII
+  , isPentagon
+  , hsGetIcosahedronFaces 
   ) where
 
-import H3.Internal.H3Api (H3Index)
+import H3.Internal.H3Api (H3Index, H3Error)
+import Foreign.C.Types (CInt)
+import System.IO.Unsafe (unsafePerformIO)
+import Foreign.Ptr (Ptr)
+import Foreign.Storable (Storable(peek))
+import Foreign.Marshal.Alloc (alloca)
+import Foreign.Marshal.Array (allocaArray, peekArray)
 
 
 -- |degsToRads converts from degrees to radians.
@@ -15,6 +26,40 @@ foreign import capi "h3/h3api.h degsToRads" degsToRads :: Double -> Double
 -- |radsToDegs converts from radians to degrees
 foreign import capi "h3/h3api.h radsToDegs" radsToDegs :: Double -> Double
 
--- |isValidCell returns non-zero if this is a valid H3 cell index
+-- Inspection
+
+-- | Returns the resolution of the index.
+foreign import capi "h3/h3api.h getResolution" getResolution :: H3Index -> Int
+
+-- | Returns the base cell number of the index.
+foreign import capi "h3/h3api.h getBaseCellNumber" getBaseCellNumber :: H3Index -> Int
+
+-- | isValidCell returns non-zero if this is a valid H3 cell index
 foreign import capi "h3/h3api.h isValidCell" isValidCell :: H3Index -> Int
+
+-- | Returns non-zero if this index has a resolution with Class III orientation.
+foreign import capi "h3/h3api.h isResClassIII" isResClassIII :: H3Index -> Int
+
+-- | Returns non-zero if this index represents a pentagonal cell.
+foreign import capi "h3/h3api.h isPentagon" isPentagon :: H3Index -> Int
+
+foreign import capi "h3/h3api.h maxFaceCount" c_maxFaceCount :: H3Index -> Ptr CInt -> IO H3Error
+
+foreign import capi "h3/h3api.h getIcosahedronFaces" c_getIcosahedronFaces :: H3Index -> Ptr CInt -> IO H3Error
+
+hsGetIcosahedronFaces :: H3Index -> (H3Error, [Int])
+hsGetIcosahedronFaces h3index = 
+  unsafePerformIO $ alloca $ \countptr -> do
+    h3error <- c_maxFaceCount h3index countptr
+    if h3error == 0
+    then do
+      count <- fromIntegral <$> peek countptr
+      allocaArray count $ \facesptr -> do
+        out3error <- c_getIcosahedronFaces h3index facesptr
+        if out3error == 0
+        then do
+          faces <- map fromIntegral <$> peekArray count facesptr
+          return (out3error, faces)
+        else return (out3error, [])
+    else return (h3error, [])
 
