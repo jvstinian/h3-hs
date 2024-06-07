587583ca3dcdd61716898f6ccca5be0a94642e5a
Justin Smith <jvstinian@gmail.com>
2024-06-07 08:31:05 -0400

    Using not and null to check for nonempty list.

--- test/RegionsTest.hs
+++ test/RegionsTest.hs
@@ -52,6 +52,6 @@ testPolygonToCells = testProperty "Testing polygonToCells" $ \(GenRadian lat) (G
 testCellsToLinkedMultiPolygon :: Test
 testCellsToLinkedMultiPolygon = testProperty "Testing cellsToLinkedMultiPolygon" $ \(GenLatLng coords) ->
     let resultE = latLngToCell coords 9 >>= (\h3index -> cellsToLinkedMultiPolygon [h3index])
-        hasAtLeastOneGeoPolygon = either (const False) ((>0) . length) resultE
+        hasAtLeastOneGeoPolygon = either (const False) (not . null) resultE
     in isRight resultE && hasAtLeastOneGeoPolygon
 
