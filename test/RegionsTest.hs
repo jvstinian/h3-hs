c5fe4cc5571f00d1bc311fd523b2290cb46a2de6
Justin Smith <jvstinian@gmail.com>
2024-06-06 22:59:15 -0400

    Adding basic test case for cellsToLinkedMultiPolygon.

--- test/RegionsTest.hs
+++ test/RegionsTest.hs
@@ -7,9 +7,11 @@ import Data.Either (isRight)
 import H3.Miscellaneous (degsToRads)
 import H3.Indexing 
   ( LatLng(LatLng)
+  , latLngToCell
   )
 import H3.Regions
   ( polygonToCells
+  , cellsToLinkedMultiPolygon
   , GeoPolygon(GeoPolygon)
   )
 import Test.Framework                       (Test, testGroup)
@@ -21,12 +23,13 @@ tests :: [Test]
 tests =
     [ testGroup "Basic functionality check"
         [ testPolygonToCells
+        , testCellsToLinkedMultiPolygon 
         ]
     ]
 
 testPolygonToCells :: Test
 testPolygonToCells = testProperty "Testing polygonToCells" $ 
-    isRight resultE
+    isRight resultE && hasAtLeastOneCell
     where
         coords_ll = LatLng (degsToRads 45 - 0.01) (degsToRads (-72.5 - 0.01))
         coords_lr = LatLng (degsToRads 45 - 0.01) (degsToRads (-72.5 + 0.01))
@@ -34,4 +37,13 @@ testPolygonToCells = testProperty "Testing polygonToCells" $
         coords_ul = LatLng (degsToRads 45 + 0.01) (degsToRads (-72.5 - 0.01))
         gp = GeoPolygon [coords_ll, coords_lr, coords_ur, coords_ul] []
         resultE = polygonToCells gp 5 0
+        hasAtLeastOneCell = either (const False) (\h3indexs -> length h3indexs > 0) resultE
+
+testCellsToLinkedMultiPolygon :: Test
+testCellsToLinkedMultiPolygon = testProperty "Testing cellsToLinkedMultiPolygon" $ 
+    isRight resultE && hasAtLeastOneGeoPolygon
+    where
+        coords = LatLng (degsToRads 45) (degsToRads (-72.5))
+        resultE = latLngToCell coords 9 >>= (\h3index -> cellsToLinkedMultiPolygon [h3index])
+        hasAtLeastOneGeoPolygon = either (const False) (\geopolys -> length geopolys > 0) resultE
 
