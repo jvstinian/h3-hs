e4591e5a7577332c0aeead3e651d4e7ca6efc682
jvstinian <jvstinian@gmail.com>
2024-06-16 14:08:27 +0000

    gridRingUnsafe array size (#14)
    * Improving array allocation size for gridRingUnsafe.
    * Bumping version.

--- src/H3/Traversal.hs
+++ src/H3/Traversal.hs
@@ -60,9 +60,10 @@ gridDiskDistancesSafe origin = toEither . hsGridDiskDistancesSafe origin
 gridDiskDistancesUnsafe :: H3Index -> Int -> Either H3ErrorCodes ([H3Index], [Int])
 gridDiskDistancesUnsafe origin = toEither . hsGridDiskDistancesUnsafe origin 
 
--- | Produces the hollow hexagonal ring centered at origin with sides of length k.
+-- | Produces the hollow hexagonal ring centered at origin with sides of length k. 
+--   In this Haskell method, zeros have been removed from the resulting list of H3 indexes.
 gridRingUnsafe :: H3Index -> Int -> Either H3ErrorCodes [H3Index]
-gridRingUnsafe h3index = toEither . hsGridRingUnsafe h3index 
+gridRingUnsafe h3index = fmap (filter (/= 0)) .  toEither . hsGridRingUnsafe h3index 
 
 -- | Given two H3 indexes, return the line of indexes between them (inclusive).
 --   This function may fail to find the line between two indexes, for example if they are very far apart. 
