46135e61470ce443f2efe588a2574b1b5599cae7
Justin Smith <jvstinian@gmail.com>
2024-06-16 08:40:00 -0400

    Updating documentation for regions and traversal.

--- src/H3/Regions.hs
+++ src/H3/Regions.hs
@@ -15,13 +15,17 @@ import H3.Internal.FFI
   )
 import H3.Internal.Utils (toEither)
 
--- | polygonToCells takes a given GeoJSON-like 'GeoPolygon' data structure and fills it with the hexagons that are contained in the 'GeoPolygon'.  
---   Containment is determined by the cells' centroids.
-polygonToCells :: GeoPolygon -> Int -> Word32 -> Either H3ErrorCodes [H3Index]
+-- | @polygonToCells@ takes a given GeoJSON-like 'GeoPolygon' data structure and fills it with the hexagons that are contained in the 'GeoPolygon'.  
+--   Containment is determined by the cells' centroids. 
+--   An argument for @flags@ is provided, which is reserved for future functionality, and should be taken to be 0 here.
+polygonToCells :: GeoPolygon -- ^ geoPolygon
+               -> Int        -- ^ res
+               -> Word32     -- ^ flags
+               -> Either H3ErrorCodes [H3Index]
 polygonToCells poly res = toEither . hsPolygonToCells poly res
 
--- | Creates 'GeoPolygon' describing the outline(s) of a set of hexagons. Polygon outlines will follow GeoJSON MultiPolygon order: 
---   Each polygon will have one outer loop, which is first in the list, followed by any holes.  
+-- | Creates 'GeoPolygon' describing the outline(s) of a set of hexagons. 
+--   Polygon outlines will have one outer loop and a list of loops representing holes.  
 --   It is expected that all hexagons in the set have the same resolution and that the set contains no duplicates. 
 --   Behavior is undefined if duplicates or multiple resolutions are present, and the algorithm may produce unexpected or invalid output.
 cellsToLinkedMultiPolygon :: [H3Index] -> Either H3ErrorCodes [GeoPolygon]
--- src/H3/Traversal.hs
+++ src/H3/Traversal.hs
@@ -31,36 +31,48 @@ import H3.Internal.FFI
   )
 import H3.Internal.Utils (toEither)
 
--- | gridDisk produces indices within k distance of the origin index.
+-- | gridDisk produces indices within @k@ distance of the @origin@ index.
 --   Elements of the output array may be left as zero, which can happen when crossing a pentagon.
-gridDisk :: H3Index -> Int -> Either H3ErrorCodes [H3Index]
+gridDisk :: H3Index -- ^ origin
+         -> Int     -- ^ k
+         -> Either H3ErrorCodes [H3Index]
 gridDisk origin = toEither . hsGridDisk origin
 
--- | gridDiskUnsafe produces indexes within k distance of the origin index. 
+-- | gridDiskUnsafe produces indexes within @k@ distance of the @origin@ index. 
 --   The function returns an error code when one of the returned by this function is a pentagon 
 --   or is in the pentagon distortion area. 
 --   In this case, the output behavior of the out array is undefined.
-gridDiskUnsafe :: H3Index -> Int -> Either H3ErrorCodes [H3Index]
+gridDiskUnsafe :: H3Index -- ^ origin 
+               -> Int     -- ^ k 
+               -> Either H3ErrorCodes [H3Index]
 gridDiskUnsafe origin = toEither . hsGridDiskUnsafe origin
 
--- | gridDiskDistances produces indices within k distance of the origin index.
+-- | gridDiskDistances produces indices within @k@ distance of the @origin@ index.
 --    k-ring 0 is defined as the origin index, k-ring 1 is defined as k-ring 0 
 --    and all neighboring indices, and so on.
-gridDiskDistances :: H3Index -> Int -> Either H3ErrorCodes ([H3Index], [Int])
+gridDiskDistances :: H3Index -- ^ origin
+                  -> Int     -- ^ k
+                  -> Either H3ErrorCodes ([H3Index], [Int])
 gridDiskDistances origin = toEither . hsGridDiskDistances origin 
 
--- | gridDiskDistancesSafe produces indexes within k distance of the origin index.
-gridDiskDistancesSafe :: H3Index -> Int -> Either H3ErrorCodes ([H3Index], [Int])
+-- | gridDiskDistancesSafe produces indexes within @k@ distance of the @origin@ index.
+gridDiskDistancesSafe :: H3Index -- ^ origin 
+                      -> Int     -- ^ k
+                      -> Either H3ErrorCodes ([H3Index], [Int])
 gridDiskDistancesSafe origin = toEither . hsGridDiskDistancesSafe origin 
 
--- | gridDiskDistancesUnsafe produces indexes within k distance of the origin index. 
+-- | gridDiskDistancesUnsafe produces indexes within @k@ distance of the @origin@ index. 
 --   Output behavior is undefined when one of the indexes returned by this function is a pentagon 
 --   or is in the pentagon distortion area.
-gridDiskDistancesUnsafe :: H3Index -> Int -> Either H3ErrorCodes ([H3Index], [Int])
+gridDiskDistancesUnsafe :: H3Index -- ^ origin
+                        -> Int     -- ^ k
+                        -> Either H3ErrorCodes ([H3Index], [Int])
 gridDiskDistancesUnsafe origin = toEither . hsGridDiskDistancesUnsafe origin 
 
--- | Produces the hollow hexagonal ring centered at origin with sides of length k.
-gridRingUnsafe :: H3Index -> Int -> Either H3ErrorCodes [H3Index]
+-- | Produces the hollow hexagonal ring centered at @origin@ with sides of length @k@.
+gridRingUnsafe :: H3Index -- ^ origin
+               -> Int     -- ^ k
+               -> Either H3ErrorCodes [H3Index]
 gridRingUnsafe h3index = toEither . hsGridRingUnsafe h3index 
 
 -- | Given two H3 indexes, return the line of indexes between them (inclusive).
@@ -68,13 +80,13 @@ gridRingUnsafe h3index = toEither . hsGridRingUnsafe h3index
 --   It may also fail when finding distances for indexes on opposite sides of a pentagon.
 --
 --   Notes:
---
---   The specific output of this function should not be considered stable across library versions. 
---   The only guarantees the library provides are that the line length will be consistent with the distance method 
---   and that every index in the line will be a neighbor of the preceding index.
---
---   Lines are drawn in grid space, and may not correspond exactly to either Cartesian lines or great arcs.
-gridPathCells :: H3Index -> H3Index -> Either H3ErrorCodes [H3Index]
+--     * The specific output of this function should not be considered stable across library versions. 
+--       The only guarantees the library provides are that the line length will be consistent with the distance method 
+--       and that every index in the line will be a neighbor of the preceding index.
+--     * Lines are drawn in grid space, and may not correspond exactly to either Cartesian lines or great arcs.
+gridPathCells :: H3Index -- ^ start
+              -> H3Index -- ^ end
+              -> Either H3ErrorCodes [H3Index]
 gridPathCells h3index = toEither . hsGridPathCells h3index
 
 -- | Provides the distance in grid cells between the two indexes.
@@ -83,20 +95,26 @@ gridPathCells h3index = toEither . hsGridPathCells h3index
 --   too far apart, 
 --   or are separated by pentagonal distortion. 
 --   This is the same set of limitations as the local IJ coordinate space functions.
-gridDistance :: H3Index -> H3Index -> Either H3ErrorCodes Int64
+gridDistance :: H3Index -- ^ origin
+             -> H3Index -- ^ destination 
+             -> Either H3ErrorCodes Int64
 gridDistance origin = toEither . c2hs_gridDistance origin
 
--- | Produces local IJ coordinates for an H3 index anchored by an origin.
+-- | Produces local IJ coordinates for an H3 index anchored by an @origin@.
 --   The C API has an additional argument mode which is reserved for future expansion and must be set to 0. 
 --   The method provided here automatically passes the value. 
 --   This function's output is not guaranteed to be compatible across different versions of H3.
-cellToLocalIj :: H3Index -> H3Index -> Either H3ErrorCodes CoordIJ
+cellToLocalIj :: H3Index -- ^ origin
+              -> H3Index -- ^ destination
+              -> Either H3ErrorCodes CoordIJ
 cellToLocalIj origin h3 = toEither $ c2hs_cellToLocalIj origin h3 0
 
--- | Produces an H3 index from local IJ coordinates anchored by an origin.
+-- | Produces an H3 index from local IJ coordinates anchored by an @origin@.
 --   The C API has an additional argument mode which is reserved for future expansion and must be set to 0, 
 --   and the method defined here automatically passes the value. 
 --   This function's output is not guaranteed to be compatible across different versions of H3.
-localIjToCell :: H3Index -> CoordIJ -> Either H3ErrorCodes H3Index
+localIjToCell :: H3Index -- ^ origin
+              -> CoordIJ -- ^ ij
+              -> Either H3ErrorCodes H3Index
 localIjToCell origin ij = toEither $ c2hs_localIjToCell origin ij 0
 
